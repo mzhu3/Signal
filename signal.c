4f3ccc65716e842e30597ae8dde31d8ea4ea4ac3
Matthew <mzhu3@stuy.edu>
2016-11-10 00:37:00 -0500

    finish

---
+++ signal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sigHandle(int sigNum){
+  if(sigNum ==SIGINT){
+    printf("SIGINT needs me, I'm out\n");
+    exit(0);
+  }
+  if(sigNum == SIGUSR1){
+    printf("My parent PID: %d\n",getppid());
+  }
+}
+int main(){
+  signal(SIGINT,sigHandle);
+  signal(SIGUSR1,sigHandle);
+  while(1){
+    printf("Gotta catch em PID: %d\n",getpid());
+    sleep(1);
+  }
+  return 0;
+}
